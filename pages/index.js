16c1fe1932cebf3aed93e908cf1b041ed6b6e187
wakfuld23 <63659885+wakfuld23@users.noreply.github.com>
2021-08-27 17:01:46 +0200

    add: input form

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,28 @@
-// Main entry point of your app
-import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 const Home = () => {
+
+  //Render methods
+  const renderForm = () => {
+    return (
+      <div className={styles.formContainer}>
+        <form>
+          <input id='name' placeholder='Twitch Channel Name' type='text' required />
+          <button type='sumbit'>Add Streamer</button>
+        </form>
+
+      </div>)
+  }
   return (
     <div className={styles.container}>
-      <Head>
+      <head>
         <title>🎥 Personal Twitch Dashboard</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
+      </head>
       <div className={styles.inputContainer}>
-        <h1>Welcome to the Personalized Twitch Dashboard! 🎉</h1>
+        {renderForm()}
       </div>
     </div>
   )
 }
-
-export default Home;
+export default Home
